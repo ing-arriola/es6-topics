94c8b5678fe4b67822b51c3189cef1adec56f5d0
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-03 11:41:49 -0600

    added destructuring arrays

--- app.js
+++ app.js
@@ -33,3 +33,19 @@ let student={
 let {education: {middleSchool}}=student
 
 console.log(middleSchool.name)
+
+//Destructuring arrays
+let cities=['Tokyo','New York','Miami','San Salvador','San Jose']
+//Get first city
+let [city]=cities
+console.log(city)
+
+//Get some cities ... both one inmediately after another
+let [city1,city2]=cities
+console.log(`city 1 is: ${city1} and city 2 is: ${city2}`)
+
+//Getting a specific city
+//As you can see you need to put "," to indicate the other values in order to be able to get 
+//the city at a specific position where you write the name of the variable that you want
+let [,,specificCity,]=cities
+console.log(specificCity)
